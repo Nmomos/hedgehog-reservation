84cc990561ecd0af2d8cb883e3a8bc8e9a813421
Kenny Song <mila_411@me.com>
2021-01-24 04:14:23 +0900

    その3: APIエンドポイントをPostgreSQLに接続

--- backend/app/api/routes/hedgehogs.py
+++ backend/app/api/routes/hedgehogs.py
@@ -1,7 +1,13 @@
 from typing import List
-from fastapi import APIRouter
+
+from app.api.dependencies.database import get_repository
+from app.db.repositories.hedgehogs import HedgehogsRepository
+from app.models.hedgehog import HedgehogCreate, HedgehogPublic
+from fastapi import APIRouter, Body, Depends
+from starlette.status import HTTP_201_CREATED
 
 router = APIRouter()
+
 
 @router.get("/")
 async def get_all_hedgehogs() -> List[dict]:
@@ -11,3 +17,15 @@ async def get_all_hedgehogs() -> List[dict]:
     ]
 
     return hedgehogs
+
+
+@router.post("/",
+             response_model=HedgehogPublic,
+             name="hedgehogs:create-hedgehog",
+             status_code=HTTP_201_CREATED)
+async def create_new_hedgehog(
+    new_hedgehog: HedgehogCreate = Body(..., embed=True),
+    hedgehogs_repo: HedgehogsRepository = Depends(get_repository(HedgehogsRepository)),
+) -> HedgehogPublic:
+    created_hedgehog = await hedgehogs_repo.create_hedgehog(new_hedgehog=new_hedgehog)
+    return created_hedgehog
